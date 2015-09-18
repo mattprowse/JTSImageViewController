656f0bd92b7bbfbbc885defa8cef9894cc6c62a5
Matt Prowse <matt@cordlessdog.com>
2015-09-18 17:03:29 +1000

    Add support for Carthage.

---
+++ Source/JTSImageViewController-Umbrella.h
@@ -0,0 +1,19 @@
+//
+//  JTSImageViewController-Umbrella.h
+//  JTSImageViewController
+//
+//  Created by Matt Prowse on 18/09/2015.
+//  Copyright © 2015 Jared Sinclair. All rights reserved.
+//
+
+#ifndef JTSImageViewController_h
+#define JTSImageViewController_h
+
+#import "JTSAnimatedGIFUtility.h"
+#import "JTSImageInfo.h"
+#import "JTSImageViewController.h"
+#import "JTSSimpleImageDownloader.h"
+#import "UIApplication+JTSImageViewController.h"
+#import "UIImage+JTSImageEffects.h"
+
+#endif /* JTSImageViewController-Umbrella_h */
